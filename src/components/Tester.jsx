ad243882d4c9315a7a045a305fde9e3a23f30850
Giao Vu Dinh <tvudinh20@gmail.com>
2023-11-18 20:01:06 -0500

    working barchart

---
+++ src/components/Tester.jsx
@@ -0,0 +1,13 @@
+// import "./App.css";
+import Barchart from "./Barchart";
+
+function Tester() {
+  return (
+    <div className="App">
+      <h1>Hello React + D3 world!</h1>
+      <Barchart />
+    </div>
+  );
+}
+
+export default Tester;
