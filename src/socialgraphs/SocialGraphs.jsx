bc25bd73d448d5f263e5cf82555e5bfda784dc9b
Giao Vu Dinh <tvudinh20@gmail.com>
2024-01-16 16:59:23 +0700

    social networks - working

--- src/socialgraphs/SocialGraphs.jsx
+++ src/socialgraphs/SocialGraphs.jsx
@@ -1,11 +1,123 @@
 // import "./App.css";
+import * as d3 from "d3";
+import { useEffect, useRef } from "react";
+
+import data from "./miserables.json";
+
+import "./styles.css";
 
 function SocialGraphs() {
-  // const [count, setCount] = useState(0)
+  const ref = useRef();
+
+  useEffect(() => {
+    // ticked function
+    function ticked() {
+      link
+        .attr("x1", (d) => d.source.x)
+        .attr("y1", (d) => d.source.y)
+        .attr("x2", (d) => d.target.x)
+        .attr("y2", (d) => d.target.y);
+
+      node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    }
+
+    // Reheat the simulation when drag starts, and fix the subject position.
+    function dragstarted(event) {
+      if (!event.active) simulation.alphaTarget(0.3).restart();
+      event.subject.fx = event.subject.x;
+      event.subject.fy = event.subject.y;
+    }
+
+    // Update the subject (dragged node) position during drag.
+    function dragged(event) {
+      event.subject.fx = event.x;
+      event.subject.fy = event.y;
+    }
+
+    // Restore the target alpha so the simulation cools after dragging ends.
+    // Unfix the subject position now that it’s no longer being dragged.
+    function dragended(event) {
+      if (!event.active) simulation.alphaTarget(0);
+      event.subject.fx = null;
+      event.subject.fy = null;
+    }
+
+    // Specify the dimensions of the chart.
+    const width = 928;
+    const height = 600;
+
+    // Specify the color scale.
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    // The force simulation mutates links and nodes, so create a copy
+    // so that re-evaluating this cell produces the same result.
+    const links = data.links.map((d) => ({ ...d }));
+    const nodes = data.nodes.map((d) => ({ ...d }));
+
+    // Create a simulation with several forces.
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force(
+        "link",
+        d3.forceLink(links).id((d) => d.id)
+      )
+      .force("charge", d3.forceManyBody())
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .on("tick", ticked);
+
+    // append SVG to page
+    const svg = d3
+      .select(ref.current)
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .attr("style", "max-width: 100%; height: auto;");
+
+    // Add a line for each link, and a circle for each node.
+    const link = svg
+      .append("g")
+      .attr("stroke", "#999")
+      .attr("stroke-opacity", 0.6)
+      .selectAll()
+      .data(links)
+      .join("line")
+      .attr("stroke-width", (d) => Math.sqrt(d.value));
+
+    const node = svg
+      .append("g")
+      .attr("stroke", "#fff")
+      .attr("stroke-width", 1.5)
+      .selectAll()
+      .data(nodes)
+      .join("circle")
+      .attr("r", 5)
+      .attr("fill", (d) => color(d.group));
+
+    node.append("title").text((d) => d.id);
+
+    // Add a drag behavior.
+    node.call(
+      d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended)
+    );
+  }, []);
 
   return (
     <>
-      <>Social Graphs Page</>
+      <h2>Social Graphs Page</h2>
+      <a href="https://observablehq.com/@d3/force-directed-graph/2?intent=fork">
+        Force Directed Graph Tutorial
+      </a>
+      <svg
+        // style={{ marginLeft: "30px" }}
+        // width={800}
+        // height={800}
+        // id="barchart"
+        ref={ref}
+      />
     </>
   );
 }
